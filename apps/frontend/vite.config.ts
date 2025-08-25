696d24adf047e9beac6a08b99a0efc953ad1e83d
semwett0301 <semen.mokrov@gmail.com>
2025-08-25 18:30:03 +0200

    fix: removed deprecated code from index.html

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,20 +3,12 @@ import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [
-      createHtmlPlugin({
-        inject: {
-          data: {
-            VITE_G_TAG: env.VITE_G_TAG,
-          },
-        },
-      }),
       tsconfigPaths(),
       checker({
         typescript: true,
