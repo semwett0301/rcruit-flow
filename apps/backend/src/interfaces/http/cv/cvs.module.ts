e75b20c66baf527da032a28e188c470c70978e77
semwett0301 <semen.mokrov@gmail.com>
2025-08-30 21:34:03 +0200

    feat: changed S3 for production environment

--- apps/backend/src/interfaces/http/cv/cvs.module.ts
+++ apps/backend/src/interfaces/http/cv/cvs.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CvsController } from 'interfaces/http/cv/cvs.controller';
-import { R2Service } from 'infrastructure/s3/minio.service';
+import { S3Service } from '../../../infrastructure/s3/s3.service';
 import { GptModule } from 'infrastructure/gpt/gpt.module';
 import { ExtractCvContentUseCase } from 'application/cv/use-case/extract-cv-content.use-case';
 import { SaveCvUseCase } from 'application/cv/use-case/save-cv.use-case';
 
 @Module({
   controllers: [CvsController],
-  providers: [ExtractCvContentUseCase, SaveCvUseCase, R2Service],
+  providers: [ExtractCvContentUseCase, SaveCvUseCase, S3Service],
   imports: [GptModule],
 })
 export class CvsModule {}
--- apps/backend/src/interfaces/http/emails/emails.module.ts
+++ apps/backend/src/interfaces/http/emails/emails.module.ts
@@ -3,11 +3,11 @@ import { GptModule } from 'infrastructure/gpt/gpt.module';
 import { ConfigModule } from '@nestjs/config';
 import { EmailsController } from 'interfaces/http/emails/emails.controller';
 import { GenerateEmailUseCase } from 'application/email/use-case/generate-email.use-case';
-import { R2Service } from 'infrastructure/s3/minio.service';
+import { S3Service } from '../../../infrastructure/s3/s3.service';
 
 @Module({
   imports: [GptModule, ConfigModule],
   controllers: [EmailsController],
-  providers: [GenerateEmailUseCase, R2Service],
+  providers: [GenerateEmailUseCase, S3Service],
 })
 export class EmailsModule {}
