bd5b50ba655ba2d5315be3d9d60b0170242af257
semwett0301 <semen.mokrov@gmail.com>
2026-01-30 00:26:32 +0100

    fix: Windows fix

--- apps/frontend/src/constants/links.ts
+++ apps/frontend/src/constants/links.ts
@@ -1,8 +1,7 @@
 export const LINKED_IN_LINK = 'https://www.linkedin.com/company/rcruitflow/';
 
-export const USER_AGREEMENT_LINK_ENG = '/docs/User agreement | ENG.pdf';
-export const TERMS_AND_AGREEMENT_LINK_ENG =
-  '/docs/Terms & Conditions | ENG.pdf';
+export const USER_AGREEMENT_LINK_ENG = '/docs/User agreement ENG.pdf';
+export const TERMS_AND_AGREEMENT_LINK_ENG = '/docs/Terms & Conditions ENG.pdf';
 
-export const USER_AGREEMENT_LINK_NL = '/docs/User agreement | NL.pdf';
-export const TERMS_AND_AGREEMENT_LINK_NL = '/docs/Terms & Conditions | NL.pdf';
+export const USER_AGREEMENT_LINK_NL = '/docs/User agreement NL.pdf';
+export const TERMS_AND_AGREEMENT_LINK_NL = '/docs/Terms & Conditions NL.pdf';
