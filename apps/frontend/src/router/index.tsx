55e11a2ab70d9c9cadb273d5e1475d4236b5ae64
semwett0301 <semen.mokrov@gmail.com>
2026-01-30 00:33:44 +0100

    fix: redirect fix

--- apps/frontend/src/router/index.tsx
+++ apps/frontend/src/router/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Navigate, useRoutes } from 'react-router-dom';
+import { BrowserRouter, useRoutes } from 'react-router-dom';
 import { IntroductionPage } from 'pages/IntroductionPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 
 const routes = [
   {
-    path: '/intromail',
-    element: <IntroductionPage />,
-  },
-  {
     path: '/',
-    element: <Navigate to="/intromail" replace />,
+    element: <IntroductionPage />,
   },
   {
     path: '*',
